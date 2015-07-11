46a1259bf319dd5c684373baa1a3eb1589b9ca2f
DoHyoungKim <jkdgbass@gmail.com>
2015-07-11 15:24:35 +0900

    기본 판때기 만들었습니다.

--- SwiftStudy/SwiftStudy/ViewController.swift
+++ SwiftStudy/SwiftStudy/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonArea = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        var nameLabel = UILabel(frame: CGRectMake(20, 20, self.view.frame.size.width - 40, 30))
+        nameLabel.textColor = UIColor.blackColor()
+        nameLabel.text = "swift study application"
+        nameLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(nameLabel)
+        
+        buttonArea = UIScrollView(frame: CGRectMake(0, 60, self.view.frame.size.width, self.view.frame.size.height - 60))
+        buttonArea.contentSize = CGSizeMake(self.view.frame.size.width, self.view.frame.size.height)
+        buttonArea.showsHorizontalScrollIndicator = false
+        buttonArea.showsVerticalScrollIndicator = false
+        self.view.addSubview(buttonArea)
+        
     }
 
     override func didReceiveMemoryWarning() {
